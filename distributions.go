12edbcfa3d6954f49677ff0e7aa5d04b22bee2c9
Ignacio Gomez <igomez9@dons.usfca.edu>
2026-01-05 20:45:57 -0800

    Fix loop range in GenerateNormalSamples function to iterate over samples slice

--- distributions.go
+++ distributions.go
@@ -2,9 +2,11 @@ package stats
 
 import "math/rand/v2"
 
+// GenerateNormalSamples generates n samples from a normal distribution
+// with the specified mean and standard deviation.
 func GenerateNormalSamples(mean, stddev float64, n int) []float64 {
 	samples := make([]float64, n)
-	for i := range n {
+	for i := range samples {
 		samples[i] = rand.NormFloat64()*stddev + mean
 	}
 	return samples
